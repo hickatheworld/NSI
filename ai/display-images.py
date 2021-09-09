00b8946e5c1f59e2724a4d8f18881ce6c97e7a10
Hicka <hello@hicka.dev>
2021-09-09 17:02:23 +0200

    Update display-images.py

--- ai/display-images.py
+++ ai/display-images.py
@@ -3,17 +3,20 @@ from pygame.locals import *
 
 
 # Data
+image_cache = [] # Cached image data
+labels_cache = [] # Cached image labels
 index = 0  # Currently displayed image index
 
-file = open('train-images.idx3-ubyte', mode='br')
-file.read(4)  # Skip magic number
-IMAGES_AMOUNT = int.from_bytes(file.read(4), byteorder='big')
+image_file = open('train-images.idx3-ubyte', mode='br')
+image_file.read(4)  # Skip magic number
+IMAGES_AMOUNT = int.from_bytes(image_file.read(4), byteorder='big')
 
-IMG_HEIGHT = int.from_bytes(file.read(4), byteorder='big')
-IMG_WIDTH = int.from_bytes(file.read(4), byteorder='big')
+IMG_HEIGHT = int.from_bytes(image_file.read(4), byteorder='big')
+IMG_WIDTH = int.from_bytes(image_file.read(4), byteorder='big')
 
-# Cached images
-cache = []
+labels_file = open('train-labels.idx1-ubyte', mode='br')
+labels_file.read(8) # Skip magic number and number of items
+
 
 # Display
 # The zoom factor to display the images
@@ -27,30 +30,34 @@ pygame.display.set_caption('MNIST reader')
 font = pygame.font.SysFont('Consolas', FONT_SIZE)
 
 
+def load_new_image():
+    global image_cache
+    label = int.from_bytes(labels_file.read(1), byteorder='big')
+    labels_cache.append(label)
+    l = []
+    for i in range(IMG_WIDTH):
+        l.append([])
+        for j in range(IMG_HEIGHT):
+            b = image_file.read(1)
+            l[i].append(int.from_bytes(b, byteorder='big'))
+    image_cache.append(l)
+
 def disp(n):
     win.fill((0, 0, 0))
-    global cache
+    global image_cache
     # Loads a new image if this index has not been reached yet
-    if (n >= len(cache)):
-        l = []
-        for i in range(IMG_WIDTH):
-            l.append([])
-            for j in range(IMG_HEIGHT):
-                b = file.read(1)
-                l[i].append(int.from_bytes(b, byteorder='big'))
-        cache.append(l)
+    if (n >= len(image_cache)):
+        load_new_image()
 
-    bytes = cache[n]
+    bytes = image_cache[n]
     # Displays the image
     for i in range(len(bytes)):
         for j in range(len(bytes[i])):
             v = bytes[i][j]
             pygame.draw.rect(win, (v, v, v), pygame.Rect(
                 j*zoom, i * zoom + 16, zoom, zoom))
-    count_text = font.render(
-        f'IMG {n + 1}/{IMAGES_AMOUNT}', True, (255, 255, 255))
-    size_text = font.render(
-        f'Base size:{IMG_WIDTH}x{IMG_HEIGHT} | Scaled to: {IMG_WIDTH * zoom}x{IMG_HEIGHT * zoom} (x{zoom})', True, (255, 255, 255))
+    count_text = font.render('IMG ' + str(n + 1) + '/' + str(IMAGES_AMOUNT) + ' Label: ' + str(labels_cache[n]), True, (255, 255, 255))
+    size_text = font.render('Base size: ' + str(IMG_WIDTH)  + 'x' + str(IMG_HEIGHT) + ' | ' + 'Scaled to: ' + str(IMG_WIDTH * zoom) + 'x' + str(IMG_HEIGHT * zoom) + ' (x' + str(zoom) + ')', True, (255, 255, 255))
     win.blit(count_text, dest=(0, 0))
     win.blit(size_text, dest=(0, IMG_HEIGHT * zoom + FONT_SIZE))
     pygame.display.flip()
