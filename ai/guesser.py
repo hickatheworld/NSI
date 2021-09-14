5d144acd69c8fe18ea420b79161337bc7193e58a
Hicka <hello@hicka.dev>
2021-09-15 00:18:55 +0200

    improve code and display

--- ai/guesser.py
+++ ai/guesser.py
@@ -1,3 +1,9 @@
+"""
+     -- Script to read and analyze data from Yann Lecun's MNIST data. --
+    Template strings are not being used because the damn school PCs are so 
+           neglected that we don't even have an up-to-date Python.
+"""
+
 import pygame
 from pygame.locals import *
 import time
@@ -26,18 +32,21 @@ files['test_images'].read(16)
 files['train_labels'].read(8)
 files['test_labels'].read(8)
 
-datasets = ['train_images', 'test_images', 'train_labels', 'test_labels']
-
+DATASETS = ['train_images', 'test_images', 'train_labels', 'test_labels']
 
 def load(dataset, amount):
-    if (dataset not in datasets):
-        print('Incorrect dataset. Can be: ', ', '.join(datasets))
+    """
+    Loads data from one of the four available datasets (test/train images/labels)
+    `amount` represents the amount of images/labels to load.
+    """
+    if (dataset not in DATASETS):
+        print('Incorrect dataset. Can be: ', ', '.join(DATASETS))
         return
     l1 = []
-    for k in range(amount):
+    for i in range(amount):
         if ('images' in dataset):
             l2 = []
-            for i in range(IMG_WIDTH * IMG_HEIGHT):
+            for j in range(IMG_WIDTH * IMG_HEIGHT):
                 b = files[dataset].read(1)
                 n = int.from_bytes(b, byteorder='big')
                 l2.append(n)
@@ -49,12 +58,19 @@ def load(dataset, amount):
     data[dataset].extend(l1)
 
 def distance(img1, img2):
+    """
+    Calculates the distance between two images.
+    """
     S = 0
     for i in range(IMG_WIDTH * IMG_HEIGHT):
         S += abs(img1[i] - img2[i])
     return S
 
 def guess(i):
+    """
+    Guesses the label of a test image using Nearest Neighboor algorithm. 
+    Returns whether the guess is correct.
+    """
     print('Guessing image n°' + str(i))
     to_guess = data['test_images'][i]
     answer = data['test_labels'][i]
@@ -71,29 +87,33 @@ def guess(i):
     return guess == answer
 
 def k_guess(i, k):
-    global guess_amount
+    """
+    Guesses the label of a test image using K Nearest Neighboors algorithm. 
+    """
     to_guess = data['test_images'][i]
-    answer = data['test_labels'][i]
-    nn = [[-1, 200000] for i in range(k)]
+    # nn is the list of the k nearest neighboors of our image (`to_guess`)
+    # every two element sublist is of the form [index of the neighboor in the train set, distance between our image and this neighboor]
+    nn = [[-1, 200000] for _ in range(k)]
     for j in range(len(data['train_images'])):
         img = data['train_images'][j]
         dist = distance(to_guess, img)
+        # nn is sorted by the distances of every sublist.
+        # Therefore, if dist is smaller than the greatest distance stored in nn, it can replace it.
         if (dist < nn[-1][1]):
             nn[-1] = [j, dist]
             nn.sort(key= lambda l: l[1])
-    labels = []
-    nn_indexes=[]
+    labels = [] # The labels of the nearest neighboors
+    nn_indexes=[] # The indexes of the nearest neighboors
     for i in range(k):
         labels.append(data['train_labels'][nn[i][0]])
         nn_indexes.append(nn[i][0])
     guess = most_frequent(labels)
-    if guess == answer:
-        global correct_guesses
-        correct_guesses+=1
-    guess_amount+=1
     return (guess, nn_indexes)
 
 def most_frequent(lst):
+    """
+    Returns the most frequent value from a list.
+    """
     counter = 0
     num = lst[0]
     for i in lst:
@@ -106,51 +126,63 @@ def most_frequent(lst):
 
 ### Graphics side
 pygame.init()
-win = pygame.display.set_mode((800, 600))
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 big_font = pygame.font.SysFont('Consolas', 50)
 small_font = pygame.font.SysFont('Consolas', 14)
+pygame.display.set_caption('MNIST')
 
 def display_guess(i):
-    global guesses
-    global correct_guesses
-    win.fill((0,0,0))
+    WIN.fill((0,0,0))
     img = data['test_images'][i]
-    disp(img, 4, (0,0))
-    if (len(guesses) == i):
+    # -> Displaying Test image
+    disp_img(img, 8, (WIN_WIDTH / 2 - 8 * IMG_WIDTH, 60))
+    if (len(guesses) == i): # if len(guesses) == i, the user is trying to access an image that has not been guessed yet.
         txt = small_font.render('Guessing...', True, (255, 255, 255))
-        win.blit(txt, dest=(0, IMG_HEIGHT * 4 + 10))
+        w = txt.get_width()
+        WIN.blit(txt, dest=(WIN_WIDTH / 2 - w / 2, IMG_HEIGHT * 8 + 60 + 10)) # 8 <=> Zoom factor ; 60 <=> Title text ; 10 <=> Margin with image
         pygame.display.flip()
         g = k_guess(i, K_FACTOR)
         guesses.append(g)
-    num = data['test_labels'][i]
     guess = guesses[i]
-    pygame.draw.rect(win, (0, 0, 0), pygame.Rect(0,IMG_HEIGHT * 4 + 10, 800, 14))
-    txt = small_font.render('Guessed: ' + str(guess[0]) + ' | Actual number: ' + str(num), True, (255, 255, 255))
-    win.blit(txt, dest=(0, IMG_HEIGHT * 4 + 10))
-    txt = small_font.render(str(K_FACTOR) + ' nearest neighboors:', True, (255, 255, 255))
-    win.blit(txt, dest=(0, IMG_HEIGHT * 4 + 30))
-    for i in range(K_FACTOR):
-        txt = small_font.render(str(data['train_labels'][guess[1][i]]), True, (255, 255, 255))
-        win.blit(txt, dest=(20 + 28*i, IMG_HEIGHT * 4 + 80))
-        disp(data['train_images'][guess[1][i]], 1, (i*IMG_WIDTH, IMG_HEIGHT * 4 + 50))
-    txt = small_font.render('Accuracy: ' + str(correct_guesses/guess_amount * 100) + '%', True, (255, 255, 255))
-    win.blit(txt, dest=(0, 500))
+    # -> Displaying Neighboors images + labels
+    for j in range(K_FACTOR):
+        full_width = K_FACTOR * 28 # The width of all the neighboor images side by side.
+        disp_img(data['train_images'][guess[1][j]], 1, (WIN_WIDTH / 2  - full_width / 2 + j * IMG_WIDTH - IMG_WIDTH / 2, IMG_HEIGHT * 8 + 60 + 50)) # 50 <=> Info texts
+        txt = small_font.render(str(data['train_labels'][guess[1][j]]), True, (255, 255, 255))
+        w = txt.get_width()
+        WIN.blit(txt, dest=(WIN_WIDTH / 2  - full_width / 2 + j * IMG_WIDTH - IMG_WIDTH / 2 + IMG_WIDTH - w / 2, IMG_HEIGHT * 8 + 60 + 50 + IMG_HEIGHT + 5)) # 5 <=> Margin with neighboor images 
+    # -> Displaying Info texts
+    guessed_num = guess[0] 
+    expected_num = data['test_labels'][i]
+    txt = small_font.render('Guessed: ' + str(guessed_num) + ' | Expected: ' + str(expected_num), True, (255, 255, 255))
+    w = txt.get_width()
+    rect(WIN_WIDTH / 2 - w / 2, IMG_HEIGHT * 8 + 60, w, 24, (0, 0, 0))
+    WIN.blit(txt, dest=(WIN_WIDTH / 2 - w / 2, IMG_HEIGHT * 8 + 60 + 10)) # 10 <=> Margin with image 
+
+
     pygame.display.flip()
 
-def disp(img, zoom, dest):
+def disp_img(img, zoom, dest):
     x = 0
     y = 0
     for v in img:
         v = 255 - v
-        pygame.draw.rect(win, (v, v, v), pygame.Rect(14 * zoom + dest[0] + x * zoom, dest[1] + y * zoom, zoom, zoom))
+        rect(14 * zoom + dest[0] + x * zoom,  dest[1] + y * zoom, zoom, zoom, (v, v, v))
         x += 1
         if x == IMG_HEIGHT:
             x = 0
             y += 1
 
+def rect(x, y, w, h, c):
+    pygame.draw.rect(WIN, c, pygame.Rect(x, y, w, h))
 
-loading_text = big_font.render('Loading images...', True, (255, 255, 255))
-win.blit(loading_text, dest=(0, 0))
+
+loading_text = big_font.render('Loading data...', True, (255, 255, 255))
+w = loading_text.get_width()
+h = loading_text.get_height()
+WIN.blit(loading_text, dest=(WIN_WIDTH / 2 - w / 2, WIN_HEIGHT / 2 - h / 2))
 pygame.display.flip()
 
 start = time.time()
@@ -162,8 +194,6 @@ load('test_labels', 1000)
 print('Loaded, took ' + str(time.time() - start) + 's')
 
 guesses = []
-correct_guesses = 0
-guess_amount = 0
 index = 0
 lock = True
 display_guess(0)
